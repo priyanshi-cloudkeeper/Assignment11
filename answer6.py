6568e57d5fc711ca5a2cc8f436e0c87cf645c5fb
priyanshi-cloudkeeper <priyanshi.tyagi@cloudkeeper.com>
2025-02-12 20:55:13 +0530

    answer6.py

--- answer6.py
+++ answer6.py
@@ -1,46 +1,29 @@
-def read_csv(file_path):
+def table(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
-    return [line.strip().split(',') for line in lines]
 
-def width(data):
-    col = len(data[0])
-    col_width = [0] * col
+    data = [line.strip().split(',') for line in lines]
 
-    for col in range(col):
-        maxi = 0
-        for row in data:
-            length = len(row[col])
-            if length > maxi:
-                maxi = length
-        col_width[col] = maxi
-    return col_width
+    col_widths = []
+    for i in range(len(data[0])):
+        max_length = max(len(row[i]) for row in data)
+        col_widths.append(max_length)
 
+    border = '+'
+    for width in col_widths:
+        border += '-' * width + '+'
 
-def table(data):
-    col_widths = width(data)
-    border = '+' + '+'.join('-' * (width) for width in col_widths) + '+'
-    
     print(border)
     for i, row in enumerate(data):
-        
         row_str = '|'
-
         for j in range(len(row)):
-    
             cell_value = row[j]
-            
             column_width = col_widths[j]
-            
-            formatted_cell = f'{cell_value:<{column_width}}'
-            
-            row_str += formatted_cell + '|'
-
+            row_str += cell_value.ljust(column_width) + '|'
         print(row_str)
-    
+
         if i == 0:
             print(border)
+    print(border)
 
-csv_file_path = "data.csv" 
-data = read_csv("/home/priyanshi/Documents/data.csv")
-table(data)
+table("/home/priyanshi/Documents/data.csv")
