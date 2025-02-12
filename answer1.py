5a75976547f9e9cc120ec6caad3b09788fb2f6cb
priyanshi-cloudkeeper <priyanshi.tyagi@cloudkeeper.com>
2025-02-12 10:22:40 +0530

    answer1.py

--- answer1.py
+++ answer1.py
@@ -1,4 +1,4 @@
-def validate_ipv4(ip):
+def ip(ip):
     parts = ip.split('.')
     if len(parts) != 4:
         print("Invalid IP")
@@ -9,34 +9,33 @@ def validate_ipv4(ip):
             print("Invalid IP")
             return
     
-    first_octet, second_octet = int(parts[0]), int(parts[1])
+    fi_oc, sec_oc = int(parts[0]), int(parts[1])
     
-    if (first_octet == 10 or
-        (first_octet == 172 and 16 <= second_octet <= 31) or
-        (first_octet == 192 and second_octet == 168)):
+    if (fi_oc == 10 or
+        (fi_oc == 172 and 16 <= sec_oc <= 31) or
+        (fi_oc == 192 and sec_oc == 168)):
         print("IP is in a private range.")
         return
     
     print(f"Valid IP: {ip}")
 
-def validate_gmail(email):
+def gmail(email):
     if "@gmail.com" not in email:
         print("Invalid Gmail address ")
         return
     
-    username = email.split('@')[0]
+    usr = email.split('@')[0]
     allowed_chars = "abcdefghijklmnopqrstuvwxyz0123456789._%+-"
     
-    for char in username:
+    for char in usr:
         if char not in allowed_chars:
             print("Invalid Gmail address format.")
             return
     
     print(f"Valid Gmail address: {email}")
 
-# Example inputs
 ip_input = input("Enter an IPaddress: ")
-validate_ipv4(ip_input)
+ip(ip_input)
 
 email_input = input("Enter a Gmail address: ")
-validate_gmail(email_input)
+gmail(email_input)
