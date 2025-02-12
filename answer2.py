d08d80e2d292fd941cd51d7bb5780b83a58913f8
priyanshi-cloudkeeper <priyanshi.tyagi@cloudkeeper.com>
2025-02-12 10:06:55 +0530

    answer2.py

--- answer2.py
+++ answer2.py
@@ -2,15 +2,15 @@ import random
 import string
 
 def generate_password():
-    uppercase = random.sample(string.ascii_uppercase, 2)
-    lowercase = random.sample(string.ascii_lowercase, 2)
-    numbers = random.sample(string.digits, 2)
-    special_chars = random.sample("!@#$%&*", 1)
+    u = random.sample(string.ascii_uppercase, 2)
+    l = random.sample(string.ascii_lowercase, 2)
+    num = random.sample(string.digits, 2)
+    special_char= random.sample("!@#$%&*", 1)
     
     all_chars = string.ascii_letters + string.digits + "!@#$%&*"
-    remaining_chars = random.sample([char for char in all_chars if char not in uppercase + lowercase + numbers + special_chars], 9)
+    remaining_char = random.sample([char for char in all_chars if char not in u + l + num + special_char], 9)
     
-    password_list = uppercase + lowercase + numbers + special_chars + remaining_chars
+    password_list = u + l + num + special_char + remaining_char
     random.shuffle(password_list)
     
     return "".join(password_list)
